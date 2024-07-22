27e243fbc32e803d74672a0f971895d5ca11565b
nicholas hung <nhung93@outlook.com>
2024-07-22 19:50:24 -0400

    now takes into account dotted notes! (eighth, quarter, and half) :D. and trying to make tests work cross-platform. also made a few edits like modify some variable names and finally close the musicxml file after writing.

--- mmp_to_musicxml/tests/test_key_signatures.py
+++ mmp_to_musicxml/tests/test_key_signatures.py
@@ -7,24 +7,20 @@ from ..converter import MMP_MusicXML_Converter
 def test_a():
 	converter = MMP_MusicXML_Converter(key_signature='a')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\a.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\a.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\a.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'a.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'a.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
 	os.remove(output)
-	
+
 def test_d():
 	converter = MMP_MusicXML_Converter(key_signature='d')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\d.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\d.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\d.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'd.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'd.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -33,11 +29,9 @@ def test_d():
 def test_gb():
 	converter = MMP_MusicXML_Converter(key_signature='gb')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\gb.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\gb.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\gb.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'gb.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'gb.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -46,11 +40,9 @@ def test_gb():
 def test_cb():
 	converter = MMP_MusicXML_Converter(key_signature='cb')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\cb.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\cb.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\cb.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'cb.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'cb.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -59,11 +51,9 @@ def test_cb():
 def test_gb_chromatic():
 	converter = MMP_MusicXML_Converter(key_signature='gb')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\gb-chromatic.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\gb-chromatic.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\gb-chromatic.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'gb-chromatic.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'gb-chromatic.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -72,11 +62,9 @@ def test_gb_chromatic():
 def test_f_chromatic():
 	converter = MMP_MusicXML_Converter(key_signature='f')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\f-chromatic.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\f-chromatic.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\f-chromatic.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'f-chromatic.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'f-chromatic.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -85,11 +73,9 @@ def test_f_chromatic():
 def test_cs():
 	converter = MMP_MusicXML_Converter(key_signature='cs')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\cs.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\cs.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\cs.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'cs.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'cs.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -98,11 +84,9 @@ def test_cs():
 def test_fs():
 	converter = MMP_MusicXML_Converter(key_signature='fs')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\fs.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\fs.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\fs.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'fs.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'fs.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -111,11 +95,9 @@ def test_fs():
 def test_fsm():
 	converter = MMP_MusicXML_Converter(key_signature='a')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\fsm.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\fsm.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\fsm.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'fsm.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'fsm.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -124,11 +106,9 @@ def test_fsm():
 def test_asm():
 	converter = MMP_MusicXML_Converter(key_signature='cs')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\asm.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\asm.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\asm.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'asm.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'asm.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -137,11 +117,9 @@ def test_asm():
 def test_gsm():
 	converter = MMP_MusicXML_Converter(key_signature='b')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\gsm.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\gsm.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\gsm.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'gsm.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'gsm.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
@@ -150,11 +128,9 @@ def test_gsm():
 def test_g_melodic_minor():
 	converter = MMP_MusicXML_Converter(key_signature='bb')
 	
-	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig\\g-minor-melodic.mmp')
-	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig/expected_output\\g-minor-melodic.xml')
-	output = os.path.join(os.path.dirname(__file__), 'test_key_sig\\g-minor-melodic.xml')
-	
-	converter.convert_file(testfile)
+	testfile = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'g-minor-melodic.mmp')
+	testfile_expected_output = os.path.join(os.path.dirname(__file__), 'test_key_sig', 'expected_output', 'g-minor-melodic.xml')
+	output = converter.convert_file(testfile)
 	
 	assert filecmp.cmp(output, testfile_expected_output, shallow=False) is True
 	
